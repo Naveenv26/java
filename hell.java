d9e29b4f3e1f084a244a61974a367f69208eb4e6
Naveenv26 <naveenv280@gmail.com>
2025-12-22 15:59:43 +0530

    Fibonacci series

--- hell.java
+++ hell.java
@@ -4,20 +4,16 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // given number is prime or !
+        // Fibonacci series
         System.out.print("enter a number");
         int num = scan.nextInt();
-        int s=0;
+        int a=-1,b=1;
+        int c;
         for(int i=1;i<=num;i++){
-            if(num%i==0){
-                s++;
-            }
-        }
-        if(s==2){
-            System.out.print(num+" is prime");
-        }
-        else{
-            System.out.print(num+" is not prime");
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
         }
 
 scan.close();
