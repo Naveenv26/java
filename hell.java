a6dc52c70cb5a9be049172e3b4d25a59630d381b
Naveenv26 <naveenv280@gmail.com>
2025-12-22 16:37:00 +0530

    sum of the entered number

--- hell.java
+++ hell.java
@@ -4,21 +4,15 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // Palindrome or not
+        // find the sum of the digits of a given number
         System.out.print("enter a number");
         int num = scan.nextInt();
-        int temp,a=0,b=num;
-        while(b!=0){
-            temp=b%10;
-            a=(a*10)+temp;
-            System.out.println(temp+"--"+a);
-            b=b/10;
-        }
-        if(num==a){
-            System.out.println("the number "+num+" is palindrome ");
-        }
-        else{
-            System.out.println("the number "+num+" is not palindrome ");
+        int a,sum=0;
+        while(num!=0){
+            a=num%10;
+            sum=a+sum;
+            num=num/10;
+            System.out.println(sum);
         }
 
 scan.close();
