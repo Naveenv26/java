e8c957da0df9676d8796c1e4710358fd39253577
Naveenv26 <naveenv280@gmail.com>
2025-12-22 17:21:03 +0530

    Armstrong number

--- hell.java
+++ hell.java
@@ -5,24 +5,21 @@ public class hell {
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
         // ind the largest number among three numbers
-        System.out.print("enter 1st number");
-        int num1 = scan.nextInt();
-        System.out.print("enter 2nd number");
-        int num2 = scan.nextInt();
-        System.out.print("enter 3rd number");
-        int num3 = scan.nextInt();
-
-        if(num1> num2 && num1>num3){
-            System.out.println("num1 is bigger");
+        System.out.print("enter a number");
+        int num = scan.nextInt();
+        int temp=num,a,sum=0,al;
+        while(temp!=0){
+            a=temp%10;
+            al=a*a*a;
+            sum=al+sum;
+            System.out.println(sum);
+            temp=temp/10;
         }
-        else if(num2> num1 && num2>num3){
-            System.out.println("num2 is bigger");
-        }
-        else if(num3> num1 && num3>num2){
-            System.out.println("num3 is bigger");
+        if(num==sum){
+            System.out.println("Armstrong number");
         }
         else{
-            System.out.println("all are equal");
+            System.out.println("not a Armstrong number");
         }
         
 scan.close();
