7244a62f2067d7b64afab0ceb16527452508d984
Naveenv26 <naveenv280@gmail.com>
2025-12-22 17:42:48 +0530

    GCD

--- hell.java
+++ hell.java
@@ -4,23 +4,20 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // ind the largest number among three numbers
-        System.out.print("enter a number");
-        int num = scan.nextInt();
-        int temp=num,a,sum=0,al;
-        while(temp!=0){
-            a=temp%10;
-            al=a*a*a;
-            sum=al+sum;
-            System.out.println(sum);
-            temp=temp/10;
-        }
-        if(num==sum){
-            System.out.println("Armstrong number");
-        }
-        else{
-            System.out.println("not a Armstrong number");
-        }
+        // GCD logic
+        System.out.print("enter a number ");
+        int num1 = scan.nextInt();
+        System.out.print("enter a number ");
+        int num2 = scan.nextInt();
+        int a=0;
+
+            for(int i=1;i<=(num1);i++){
+                if(num2%i==0 && num1%i==0){
+                    a=i;
+                }
+            }
+        System.out.println(a);
+        
         
 scan.close();
 
