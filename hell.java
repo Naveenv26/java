add5142e9578f36d12503bed563fb3b05ed6d9ce
Naveenv26 <naveenv280@gmail.com>
2025-12-22 16:28:46 +0530

    palindrome or not

--- hell.java
+++ hell.java
@@ -4,16 +4,21 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // Fibonacci series
+        // Palindrome or not
         System.out.print("enter a number");
         int num = scan.nextInt();
-        int a=-1,b=1;
-        int c;
-        for(int i=1;i<=num;i++){
-            c=a+b;
-            System.out.println(c);
-            a=b;
-            b=c;
+        int temp,a=0,b=num;
+        while(b!=0){
+            temp=b%10;
+            a=(a*10)+temp;
+            System.out.println(temp+"--"+a);
+            b=b/10;
+        }
+        if(num==a){
+            System.out.println("the number "+num+" is palindrome ");
+        }
+        else{
+            System.out.println("the number "+num+" is not palindrome ");
         }
 
 scan.close();
