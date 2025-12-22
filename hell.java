94bbbc3519337adf3bd9a6594cb461579075eb17
Naveenv26 <naveenv280@gmail.com>
2025-12-22 16:58:53 +0530

    largest number amoung 3numbers

--- hell.java
+++ hell.java
@@ -4,17 +4,28 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // find the sum of the digits of a given number
-        System.out.print("enter a number");
-        int num = scan.nextInt();
-        int a,sum=0;
-        while(num!=0){
-            a=num%10;
-            sum=a+sum;
-            num=num/10;
-            System.out.println(sum);
-        }
+        // ind the largest number among three numbers
+        System.out.print("enter 1st number");
+        int num1 = scan.nextInt();
+        System.out.print("enter 2nd number");
+        int num2 = scan.nextInt();
+        System.out.print("enter 3rd number");
+        int num3 = scan.nextInt();
 
+        if(num1> num2 && num1>num3){
+            System.out.println("num1 is bigger");
+        }
+        else if(num2> num1 && num2>num3){
+            System.out.println("num2 is bigger");
+        }
+        else if(num3> num1 && num3>num2){
+            System.out.println("num3 is bigger");
+        }
+        else{
+            System.out.println("all are equal");
+        }
+        
 scan.close();
+
         }
     }
