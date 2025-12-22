b09917914a3f0bf48cf6612ae9aa663f681bf4c6
Naveenv26 <naveenv280@gmail.com>
2025-12-22 18:07:25 +0530

    Reverse a number

--- hell.java
+++ hell.java
@@ -4,22 +4,16 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // GCD logic
+        // Reverse a number
         System.out.print("enter a number ");
         int num = scan.nextInt();
-        int sum=0;
-
-        for(int i=1;i<=num-1;i++){
-            if(num%i==0){
-                sum=i+sum;
-            }
+        int temp,rev=0;
+        while(num!=0){
+            temp=num%10;
+            rev=(rev*10)+temp;
+            num=num/10;
         }
-        if(num==sum){
-            System.out.println(num+" perfect value");
-        }
-        else{
-            System.out.println(num + " not perfect");
-        }
+        System.out.println(rev);
 
 
 scan.close();
