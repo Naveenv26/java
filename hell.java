9a0ffca3e1b2f510d89e48e0e55dd796fb8602ca
Naveenv26 <naveenv280@gmail.com>
2025-12-22 22:40:32 +0530

    Pattern printing(*)

--- hell.java
+++ hell.java
@@ -4,20 +4,15 @@ public class hell {
 
     public static void main(String[] args) {
         Scanner scan=new Scanner(System.in);
-        // LCM
+        // pattern printing
         System.out.print("enter a number ");
         int num1 = scan.nextInt();
-        System.out.print("enter a number ");
-        int num2 = scan.nextInt();
-        int val=0,lcm;
         for(int i=1;i<=num1;i++){
-            if(num1%i==0 && num2%i==0){
-                val=i;
-                System.out.println(val);
+            System.out.println(" ");
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
             }
         }
-        lcm=(num1*num2)/val;
-        System.out.println(lcm);
 scan.close();
 
         }
