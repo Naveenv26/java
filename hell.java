09c959613aee354d0a0ae5f079ab8e8cc0d133b6
Naveenv26 <naveenv280@gmail.com>
2025-12-25 17:09:40 +0530

    max value arr , sort

--- hell.java
+++ hell.java
@@ -2,11 +2,18 @@ public class Hell {
 
 
     public static void main(String[] args) {//adding array nums
-        int sum=0;
-        int[]a=new int[]{5,10,15,20};
+        int temp;
+        int[]a=new int[]{12,45,7,89,23};
         for(int i=0;i<(a.length);i++){
-            sum=a[i]+sum;
+            for(int j=i;j<(a.length-1);j++){
+                System.out.println(a[i]+"--"+a[j+1]);
+                if(a[i]>a[j+1]){
+                    temp=a[i];
+                    a[i]=a[j+1];
+                    a[j+1]=temp;
+                }
+            }
         }
-        System.out.println(sum);
+        System.out.print("Maximum value : "+a[a.length-1]);
     }
     }
