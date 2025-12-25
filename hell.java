567eb9c8dcee1ecf08c3e31657d4c120607dcb83
Naveenv26 <naveenv280@gmail.com>
2025-12-25 17:15:21 +0530

    max num better optimized

--- hell.java
+++ hell.java
@@ -2,18 +2,15 @@ public class Hell {
 
 
     public static void main(String[] args) {//adding array nums
-        int temp;
+        
         int[]a=new int[]{12,45,7,89,23};
-        for(int i=0;i<(a.length);i++){
-            for(int j=i;j<(a.length-1);j++){
-                System.out.println(a[i]+"--"+a[j+1]);
-                if(a[i]>a[j+1]){
-                    temp=a[i];
-                    a[i]=a[j+1];
-                    a[j+1]=temp;
-                }
+        int max=a[0];
+        for(int i=1;i<(a.length);i++){
+            if(a[i]>max){
+                max=a[i];
+
             }
         }
-        System.out.print("Maximum value : "+a[a.length-1]);
+        System.out.print("Maximum value : "+max);
     }
     }
