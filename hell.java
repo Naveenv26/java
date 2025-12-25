ce6fc17f5a7f7d9400343a2a4b74726455989e38
Naveenv26 <naveenv280@gmail.com>
2025-12-25 17:56:18 +0530

    reverse array

--- hell.java
+++ hell.java
@@ -1,16 +1,14 @@
 public class Hell {
 
+    public static void main(String[] args) {
+        int []a=new int[]{1,2,3,4,5};
+        int[]b;
 
-    public static void main(String[] args) {//adding array nums
-        
-        int[]a=new int[]{12,45,7,89,23};
-        int max=a[0];
-        for(int i=1;i<(a.length);i++){
-            if(a[i]>max){
-                max=a[i];
+        for(int i=a.length-1;i>=0;i--){
+            System.out.println(a[i]);
+            
 
-            }
         }
-        System.out.print("Maximum value : "+max);
+        
     }
-    }
+}
