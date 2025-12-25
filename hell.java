06bcb60c9d86db7552786adf8545cceae75d5cf0
Naveenv26 <naveenv280@gmail.com>
2025-12-25 14:25:11 +0530

    better understanding of const

--- hell.java
+++ hell.java
@@ -1,13 +1,9 @@
 public class Hell {
-    static int track;
-    Hell(){
-        this.track=track+1;
-        System.out.println(track);
+    
 
-    }
     public static void main(String[] args) {
-        Hell a=new Hell();
-        Hell a1=new Hell();
-        Hell a2=new Hell();
+        int []a;
+        a=new int[5];
+        System.out.println(a[0]);
     }
-}
+    }
