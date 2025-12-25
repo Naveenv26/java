1c1c6e2c7c28ae5408d4a52ae7a56820f7e5c765
Naveenv26 <naveenv280@gmail.com>
2025-12-25 13:38:01 +0530

    Constructor

--- hell.java
+++ hell.java
@@ -1,47 +1,13 @@
-public class hell {
+public class Hell {
+
+    Hell(int id,String name,int salary){
+        System.out.println("ID : "+id);
+        System.out.println("Name : "+name);
+        System.out.println("salary : "+salary);
+    }
 
     public static void main(String[] args) {
-        bankaccount bank=new bankaccount();
-        bankaccount bank1=new bankaccount();
-        bank.account=666;
-        bank.balance=2000;
-        bank.name="naveen";
-        bank.display();
-        bank.deposit(500);
-        bank.display();
-        bank1.account=896;
-        bank1.balance=1000;
-        bank1.name="thors";
-        bank1.display();
-        bank1.withdraw(6000);
-        bank1.display();
-        
-    }
-}
-
-class bankaccount{
-    String name;
-    int account,balance;
-
-    void deposit(int amount){
-        balance= (balance+amount);
-    }
-    void withdraw(int amount){
-        if(balance>=amount){
-            balance=balance-amount;
-        }
-        else{
-            System.out.println("insufficient amount");
-            System.out.println("-------------****-----------");
-        }
-        
-    }
-    void display(){
-        System.out.println("holder name :"+name);
-        System.out.println("ACC NO :"+account);
-        System.out.println("Balance :"+balance);
-        System.out.println("------------------------");
+        Hell emp =new Hell(101,"Naveen",25000);
 
     }
-
 }
