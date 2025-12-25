6605da4ed23a276c78e277f60b6cd584728b3e47
Naveenv26 <naveenv280@gmail.com>
2025-12-25 18:17:20 +0530

    reverse a string in an array

--- hell.java
+++ hell.java
@@ -2,13 +2,19 @@ public class Hell {
 
     public static void main(String[] args) {
         int []a=new int[]{1,2,3,4,5};
-        int[]b;
+        int[]b=new int[5];
+        int temp,j=0;
 
         for(int i=a.length-1;i>=0;i--){
-            System.out.println(a[i]);
-            
 
-        }
+            temp=a[i];
+            b[j]=temp;
+            j=j+1;
+                //System.out.println(b[j]);
+            }
         
+        for(int t=0;t<5;t++){
+            System.out.println(b[t]);
+        }
     }
 }
