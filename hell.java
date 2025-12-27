57e80fe0e04935f70ecf4ee11b31fe61496aee98
Naveenv26 <naveenv280@gmail.com>
2025-12-27 18:17:45 +0530

    inheritance

--- hell.java
+++ hell.java
@@ -1,20 +1,29 @@
 public class Hell {
 
+    Hell(){
+        //constructor
+
+    }
+
     public static void main(String[] args) {
-        int []a=new int[]{1,2,3,4,5};
-        int[]b=new int[5];
-        int temp,j=0;
-
-        for(int i=a.length-1;i>=0;i--){
-
-            temp=a[i];
-            b[j]=temp;
-            j=j+1;
-                //System.out.println(b[j]);
-            }
-        
-        for(int t=0;t<5;t++){
-            System.out.println(b[t]);
-        }
+        child s=new child();
+        s.display();
     }
 }
+
+class Parent{
+    void display(){
+        System.out.println("parent constructor called");
+    }
+}
+
+class child extends Parent{
+    void display(){
+        System.out.println("orewa child uu");
+    }
+    
+
+}
+
+
+
