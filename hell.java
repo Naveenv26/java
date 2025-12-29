676daccee5b51e18edefd4434ff65fbed7b7e4e6
Naveenv26 <naveenv280@gmail.com>
2025-12-29 15:03:27 +0530

    Abstract easy

--- hell.java
+++ hell.java
@@ -1,40 +1,46 @@
 public class Hell {
 
-    Hell(){
-        //constructor
-
-    }
-
     public static void main(String[] args) {
-
-        Account a=new Saving(5000);
-        a.show();
-    }
-}
-
-class Account{
-
-    Account(int balance){
-        this.balance=balance;
-    }
-    int balance;
-    void show(){
-        System.out.println(balance);
-    }
-
-}
-
-class Saving extends Account{
-    
-    Saving(int balance){
-        super(balance);
-        System.out.println(balance);
+        payment pp=new creditcard(5000);
+        payment pay=new paypal(2000);
         
-    }
-    
-    void show(){
-        super.show();
+        pay.processpayment();
+        pay.detais();
+        pp.processpayment();
+        pp.detais();
     }
 }
 
+abstract class payment{
+    int Amount;
+    payment(int Amount){
+        this.Amount=Amount;
+    }
+    abstract void processpayment();
+            
+        void detais(){
+        System.out.println("Amount : "+ this.Amount);
+    
+    }
+}
 
+class creditcard extends payment{
+
+    creditcard(int Amount){
+        super(Amount);
+    }
+    
+    @Override
+    void processpayment() {
+        System.out.println("card processed");
+    }
+}
+
+class paypal extends payment{
+    paypal(int Amount){
+        super(Amount);
+    }
+    void processpayment(){
+        System.out.println("paypal processed");
+    }
+}
