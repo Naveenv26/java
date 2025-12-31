837b60911885993b0b10165ac84c0f48c4da04f2
Naveenv26 <naveenv280@gmail.com>
2025-12-31 18:48:30 +0530

    hash set

--- cal.java
+++ cal.java
@@ -1,26 +1,16 @@
-import java.util.HashMap;
+import java.util.ArrayList;
+
+import java.util.HashSet;
 
 public class cal {
 
     public static void main(String[] args) {
-        HashMap<String,Integer> vb=new HashMap<>();
-        vb.put("apple", 50);
-        vb.put("bamama", 30);
+        ArrayList<String> email=new ArrayList<>();
+        email.add("a@test.com");
+        email.add("b@test.com");
+        email.add("a@test.com");
 
-        if(vb.containsKey("mango")){
-            int a=vb.get("mango");
-            System.out.println("mango stock is : "+a);
-        }
-        else{
-            System.out.println("mango out of stock");
-        }
-
-        if (vb.containsKey("apple")){
-            int b=vb.get("apple");
-            System.out.println("apple's stock is : "+b);
-        }
-        else{
-            System.out.println("apple out of stock");
-        }
+        HashSet<String> aa=new HashSet<>(email);
+        System.out.println(aa);
     }
 }
