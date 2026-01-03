a5100ec41bcb8ca3386b6435e08f7bb64ad83806
Naveenv26 <naveenv280@gmail.com>
2026-01-03 21:27:18 +0530

    core java completed i guess

--- Mart.java
+++ Mart.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
+
 public class Mart {
 
     public static void main(String[] args) {
-        Electronics e=new Electronics(101, "wire", 20,2);
-        Cloths c=new Cloths(201, "T-Shirt", 299, "M",5);
-        e.Display();
-        c.Display();
+        Electronics e=new Electronics(101, "wire", 20,2,10);
+        Cloths c=new Cloths(201, "T-Shirt", 299, "M",5,50);
+        e.display();
+        c.display();
+        
 
+
+        user u=new user(200, "rag ");
+        
+        try {
+            u.addtocart(c);
+            u.addtocart(e);
+        } 
+        catch (outofstockException h) {
+            System.out.println(h);
+        }
+        
+        u.calculatetotal();
+        u.checkout( new Credit() );
     }
 }
 
@@ -18,22 +34,31 @@ abstract class Product implements Discount{
     private int id;
     private String name;
     private double price;
+    
+    int stock;
 
-    Product(int id, String name, double price){
+    Product(int id, String name, double price, int stock){
         this.id=id;
         this.name=name;
         this.price=price;
+        
+        this.stock=stock;
     }
-    abstract void Display();
-
+    abstract void display();
+    
     int id(){
         return id;
     }
     String name(){
         return name;
     }
+    
+    
     double price(){
         return price;
+    }
+    double price(double percentage){
+        return calculatediscount(percentage);
     }
 
     @Override
@@ -44,14 +69,18 @@ abstract class Product implements Discount{
 
 class Electronics extends Product{
     int Warrenty;
-
-    Electronics(int id,String name,double price,int Warrenty){
-        super(id, name, price);
+    int percentage;
+    Electronics(int id,String name,double price,int Warrenty,int stock){
+        super(id, name, price,stock);
         this.Warrenty=Warrenty;
+    }
+        @Override
+    double price() {
+    return calculatediscount(percentage);
     }
 
     @Override
-    public void Display(){
+    public void display(){
         System.out.println("Name : "+name());
         System.out.println("Price : "+price());
         System.out.println("Warrenty(months) : "+Warrenty);
@@ -61,18 +90,86 @@ class Electronics extends Product{
 class Cloths extends Product{
     String size;
     double percentage;
-    Cloths(int id,String name,double price,String size,double percentage){
-        super(id,name,price);
+    Cloths(int id,String name,double price,String size,double percentage,int stock){
+        super(id,name,price,stock);
         this.size=size;
         this.percentage=percentage;
     }
 
+    // Inside class Cloths
+    @Override
+    double price() {
+    return calculatediscount(percentage);
+    }
+
 
     @Override
-    void Display() {
+    void display() {
         System.out.println("Name : "+name());
         System.out.println("Price : "+price());
-        System.out.println("Price after Discount : "+calculatediscount(percentage));
         System.out.println("Size : "+size);
+    }
+}
+
+class user{
+    int userid;
+    String username;
+    ArrayList<Product> cart=new ArrayList<>();
+
+    user(int userid,String username){
+        this.userid=userid;
+        this.username=username;
+        
+    }
+
+    void addtocart(Product p) throws outofstockException{
+        
+        if(p.stock>0){
+            cart.add(p);
+            p.stock=p.stock-1;
+        }
+        else{
+            throw new outofstockException();
+        }
+        System.out.println("product Added "+p);
+    }
+
+    double calculatetotal(){
+        double sum=0;
+        for(Product p: cart){
+            sum=p.price()+sum;
+        }
+        return sum;
+    }
+
+    void checkout(paymentmethod pm){
+        double Total=calculatetotal();
+        System.out.println("Total amount :"+Total);
+        pm.pay(Total);
+        cart.clear();
+        System.out.println("Order placed");
+    }
+}
+
+class outofstockException extends Exception{}
+
+
+interface paymentmethod{
+    void pay(double amount);
+}
+
+class Credit implements paymentmethod{
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("amount : "+ amount+"paid via credid card");
+    }
+}
+
+class UPI implements paymentmethod{
+
+    @Override
+    public void pay(double amount) {
+        System.out.println("amount :"+amount+" paid via UPI");
     }
 }
