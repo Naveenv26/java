ad615551daca3ddf4da43fa31117bc62da3c4a95
Naveenv26 <naveenv280@gmail.com>
2026-01-04 17:36:47 +0530

    Advanced i guess

--- hell.java
+++ hell.java
@@ -1,44 +1,20 @@
 public class Hell {
 
     public static void main(String[] args) {
-        smartphone s=new smartphone("apple");
-        s.turnon();
-        s.takephoto();
-        s.playsong();
+        Storage<Integer> a=new Storage<>(20);
+        Storage<String> b=new Storage<>("alakazam");
+        a.display();
+        b.display();
     }
 }
 
-abstract class Device{
-    String name;
-    Device(String name){
-        this.name=name;
+class Storage<T>{
+    T item;
+
+    Storage(T item){
+        this.item=item;
     }
-    abstract void turnon();
+    void display(){
+        System.out.println(item);
+    }
 }
-
-interface camera{
-    void takephoto();
-}
-
-interface musicplayer {
-
-    void playsong();
-}
-
-class smartphone extends Device implements camera ,musicplayer{
-    smartphone(String name){
-        super(name);
-        
-    }
-    void turnon(){
-        System.out.println("Device turing ON ");
-        System.out.println("WELCOME TO "+name);
-    }
-    public void takephoto(){
-        System.out.println("photo taken");
-    }
-    
-    public void playsong(){
-        System.out.println("playing song");
-    }
-    }
